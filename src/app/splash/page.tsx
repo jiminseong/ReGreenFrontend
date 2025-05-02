439353f6d3b8ba983e748f2c2b8a6ac0fde51d25
jiminseong <iamjms4237@gachon.ac.kr>
2025-05-03 02:03:01 +0900

    refactor: simplify splash page logic and improve transition duration

--- src/app/splash/page.tsx
+++ src/app/splash/page.tsx
@@ -1,23 +1,18 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
 const SplashPage = () => {
-  const [isVisible, setIsVisible] = useState(true);
   const router = useRouter();
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsVisible(false);
-      setTimeout(() => router.push(`/onboard`), 1000);
-    }, 3000);
+    const timer = setTimeout(() => router.push(`/onboard`), 1500);
+
     return () => clearTimeout(timer);
   }, [router]);
-
-  if (!isVisible) return null;
 
   return (
     <motion.div
@@ -25,7 +20,7 @@ const SplashPage = () => {
       initial={{ opacity: 1 }}
       animate={{ opacity: 0 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 1 }}
+      transition={{ duration: 2 }}
     >
       <Image src="/192.webp" alt="로고" width={100} height={100} />
       <h1 className="text-2xl font-bold mt-4">wooimi</h1>
