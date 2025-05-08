3d986128e4d2f1e31f6af771966b13a78e5e1a50
jiminseong <iamjms4237@gachon.ac.kr>
2025-05-08 16:12:26 +0900

    feat: implement LoginButton component and update LoginPage for improved login functionality

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
+import LoginButton from "@/features/auth/ui/LoginButton";
 
 const LoginPage = () => {
   const router = useRouter();
@@ -27,34 +27,9 @@ const LoginPage = () => {
 
       <div className=" w-full items-center justify-center flex flex-col gap-4">
         {/* 카카오 로그인 버튼 */}
-        <button
-          className="flex items-center justify-center w-full max-w-xs px-4 py-3  bg-[#F7CE45] border-[1px] border-[#E2E2E2]  font-semibold rounded-xl "
-          onClick={() => router.push("/home")}
-        >
-          <Image
-            src="/icon/auth/kakao.svg"
-            alt="카카오 로고"
-            width={14.87}
-            height={14.87}
-            className="mr-3"
-          />
-          카카오 계정으로 로그인
-        </button>
-
+        <LoginButton provider="kakao" />
         {/* 네이버 로그인 버튼 */}
-        <button
-          className="flex items-center justify-center w-full max-w-xs px-4 py-3  bg-white border-[1px] border-[#E2E2E2]  font-semibold rounded-xl "
-          // onClick={() => router.push("/auth/naver")}
-        >
-          <Image
-            src="/icon/auth/naver.svg"
-            alt="네이버 로고"
-            width={12.79}
-            height={12.79}
-            className="mr-3"
-          />
-          네이버 계정으로 로그인
-        </button>
+        <LoginButton provider="naver" />
       </div>
       {/* 하단 텍스트 */}
       <button className="underline text-sm text-gray-500 mt-4">로그인에 어려움이 있나요?</button>
---
+++ src/features/auth/ui/LoginButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+
+interface LoginButtonProps {
+  provider?: "kakao" | "naver";
+}
+
+const LoginButton: React.FC<LoginButtonProps> = ({ provider = "kakao" }) => {
+  // const router = useRouter();
+  function handleLogin() {}
+  return (
+    <button
+      className={`${
+        provider === "kakao" ? "bg-[#F7CE45] " : "bg-white "
+      }  flex items-center justify-center w-full max-w-xs px-4 py-3  border-[1px] border-[#E2E2E2]  font-semibold rounded-xl `}
+      onClick={() => handleLogin()}
+    >
+      <Image
+        src={`/icon/auth/${provider}.svg`}
+        alt={`${provider}로그인`}
+        width={14.87}
+        height={14.87}
+        className="mr-3"
+      />
+      {`${provider === "kakao" ? "카카오" : "네이버"}`} 계정으로 로그인
+    </button>
+  );
+};
+
+export default LoginButton;
