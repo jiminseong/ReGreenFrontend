86009b2ae49273cf9d82b180c44ec8fd11aa2afd
jiminseong <iamjms4237@gachon.ac.kr>
2025-05-11 19:55:50 +0900

    feat: update onboarding components with new images and adjust layout styles

--- src/widgets/onboard/OnboardContainer3.tsx
+++ src/widgets/onboard/OnboardContainer3.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const OnboardContainer3 = () => {
   return (
     <div className="flex flex-col gap-8 items-center justify-center">
-      <h1 className="text-2xl font-bold text-center">
+      <h1 className="absolute text-2xl font-bold text-center top-[101px]">
         인증 후 받은 보상으로
         <br />
         우리만의 아지트를 꾸며보자!
@@ -11,9 +11,9 @@ const OnboardContainer3 = () => {
       <Image
         src="/image/onboard/3.png"
         alt="온보딩 이미지"
-        width={259}
-        height={338}
-        className="mb-4"
+        width={326}
+        height={444}
+        className="absolute bottom-[117px] z-0"
       />
     </div>
   );
