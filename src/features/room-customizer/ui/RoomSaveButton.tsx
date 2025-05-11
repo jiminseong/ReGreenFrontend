b8bd3e6500ba09b954f9a4417419a83514e03d25
jiminseong <iamjms4237@gachon.ac.kr>
2025-05-11 20:50:30 +0900

    feat: add RoomSaveButton with modal functionality and implement CommonModal component

--- src/features/room-customizer/ui/RoomSaveButton.tsx
+++ src/features/room-customizer/ui/RoomSaveButton.tsx
@@ -3,8 +3,16 @@ import { motion } from "framer-motion";
 import React from "react";
 
 import { useHomeMode } from "../lib/useHomeMode";
+import CommonModal from "@/widgets/ComonModal";
 
 const RoomSaveButton = () => {
+  const [modal, setModal] = React.useState(false);
+
+  //모달을 여닫는 함수
+  const handleModal = () => {
+    setModal(!modal);
+  };
+
   const handleHomeMode = () => {
     if (mode === "inventory") {
       setMode("home");
@@ -12,20 +20,31 @@ const RoomSaveButton = () => {
   };
   const { mode, setMode } = useHomeMode();
   return (
-    <motion.button
-      onClick={handleHomeMode}
-      whileHover={{ scale: 1.05 }}
-      animate={{
-        opacity: mode === "inventory" ? 1 : 0,
-        pointerEvents: mode === "inventory" ? "auto" : "none",
-      }}
-      transition={{ duration: 0.3, ease: "easeInOut" }}
-      className={`${
-        mode === "inventory" ? "visible" : "hidden"
-      } font-extrabold px-4   text-lg rounded-lg`}
-    >
-      저장
-    </motion.button>
+    <>
+      <motion.button
+        onClick={() => {
+          handleHomeMode();
+          handleModal();
+        }}
+        whileHover={{ scale: 1.05 }}
+        animate={{
+          opacity: mode === "inventory" ? 1 : 0,
+          pointerEvents: mode === "inventory" ? "auto" : "none",
+        }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        className={`${
+          mode === "inventory" ? "visible" : "hidden"
+        } font-extrabold px-4   text-lg rounded-lg`}
+      >
+        저장
+      </motion.button>
+      <CommonModal
+        isOpen={modal}
+        onCancel={handleModal}
+        onConfirm={handleModal}
+        message={"아직 구매하지 않은\n상품이 있어요!"}
+      />
+    </>
   );
 };
 
