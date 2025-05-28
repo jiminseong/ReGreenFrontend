9221c7b3208ab44043821969e33874bb6b8aec4a
jiminseong <iamjms4237@gachon.ac.kr>
2025-05-29 00:33:54 +0900

    refactor: update postCertification to use 'body' instead of 'json' for formData

--- src/features/certification/lib/postCertification.tsx
+++ src/features/certification/lib/postCertification.tsx
@@ -5,7 +5,7 @@ export const postCertification = async (id: string, formData: FormData) => {
   try {
     const res = await http
       .post(`api/eco-verifications/${id}`, {
-        json: formData,
+        body: formData,
         headers: undefined, //ky가 content-type 자동 설정하도록 유도
       })
       .json<PostCertificationResponse>();
