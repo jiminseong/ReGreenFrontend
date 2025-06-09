844af43a53fec440ac7b9c6899c043e8a25e60cc
jiminseong <iamjms4237@gachon.ac.kr>
2025-06-10 03:37:07 +0900

    refactor: remove dom-to-image-more dependency and related type definitions from project

--- src/dom-to-image-more.d.ts
+++
@@ -1,9 +0,0 @@
-// dom-to-image-more.d.ts
-declare module "dom-to-image-more" {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  import { Options as DomToImageOptions, toBlob as toBlobFn } from "dom-to-image-more";
-  // 최소한 아래 시그니처만 선언해 주면 일반적인 toBlob 사용은 타입체크 없이 되도록 할 수 있습니다.
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  export function toBlob(node: Node, options?: any): Promise<Blob>;
-  export default { toBlob } as { toBlob: typeof toBlobFn };
-}
